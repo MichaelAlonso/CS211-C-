5a0a393b166f73d25ee81485b22140aab8ac5f06
Michael Alonso <53191623+MichaelAlonso@users.noreply.github.com>
2022-11-03 19:01:46 -0700

    Add files via upload

---
+++ llClass/LL.h
@@ -0,0 +1,42 @@
+#ifndef LL_H
+#define LL_H
+#include <string>
+typedef int el_t;
+using namespace std;
+
+struct Node {
+  el_t elem;
+  Node* next;
+};
+
+class LL 
+{
+ private:
+  Node* front;
+  Node* rear;
+  int count;
+  void llError(string errorMessage);
+
+ public:
+  LL();
+  ~LL();
+  
+  void addRear(el_t el);
+  el_t deleteFront();
+  bool isEmpty();
+  void displayAll();
+  
+  //the functions for the next part of the lab using LL
+  el_t deleteRear();
+  void addFront(el_t el); 
+  LL(const LL& source);
+
+  //moreOnLL functions
+  bool search(el_t e);
+  void deleteNode(el_t e);
+  void deleteNodes(el_t e); //this is extra credit if i have time ill work on it
+  void addInOrderAscend(el_t e);
+  void addInOrderDescend(el_t e); //this is extra credit if i have time ill work on it
+};
+
+#endif
